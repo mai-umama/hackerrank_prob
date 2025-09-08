e7fb934a11a861b63b2e9fe7ce76f55f93e3062a
Maisha Osman Umama <umamau418@gmail.com>
2025-09-08 15:29:11 +0600

    for python 2

---
+++ python/tuple.py
@@ -0,0 +1,5 @@
+n = int(input())
+integer_list = tuple(map(int,input().split())) #in python 2 to get the correct answer it need to written raw_input
+print(hash(integer_list))
+#this code only gives correct ans for python 2
+
