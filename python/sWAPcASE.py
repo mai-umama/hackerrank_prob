635940e7a7d0f933f9c542cd46b4ba510a5350e9
Maisha Osman Umama <umamau418@gmail.com>
2025-09-09 21:23:04 +0600

    add some comments

--- python/sWAPcASE.py
+++ python/sWAPcASE.py
@@ -1,6 +1,10 @@
-def swap_case(s):
-    return s.swapcase()
-if__name__ = '__main__'
+def swap_case(s): #define a function which has a perameter s
+    return s.swapcase() #*** the main fuction which convert the cases and and return the result
+# it store the valur in result variable and print
+if__name__ = '__main__' # it only runs when it is a direct function and not imported . 
+#if the function imported then the code doesn'run
 s = input()
-result = swap_case(s)
+result = swap_case(s) #function call and returned value assigned
 print(result)
+#return mane value ta function r baire pathao 
+#ex: s.swapcase() a HEllo boshe geche and result a store hoyeche
