daf8b591efa6f48a7510cbf55b73a153f31ec7a2
Maisha Osman Umama <umamau418@gmail.com>
2025-09-14 01:19:43 +0600

    corection

--- python/capilazed.py
+++ python/capilazed.py
@@ -1,6 +1,6 @@
 import os # os is a module which is not imported . firstly need to import the module
 def solve(s):
-    return " ".join(word.capitalize() for word in s.split(" ")) # it is also correct with s.title() but i test case 
+    return " ".join(word.capitalize() for word in s.split(" ")) # it is also correct with s.title() but 1 test case 
     # failed so we need to write this
 
 if __name__ == '__main__':
