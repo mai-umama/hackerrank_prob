e583440ba58ebf4242c85d87d9a8d6c3624d3db0
Maisha Osman Umama <umamau418@gmail.com>
2025-09-18 13:32:59 +0600

    Add comments for better understanding

--- python/set_add().py
+++ python/set_add().py
@@ -1,8 +1,7 @@
 N = int(input())
-country = set()
-
+country = set()# the set is empty here by the next add() method the elements added
 for i in range(N):
-    contries = input().strip()
-    country.add(contries)
+    contries = input().strip() # strip is important cause if we input " uk " we nwwd "uk" without spaces strip function erase the spaces automiataclly
+    country.add(contries) #add the countries on country set 
 
 print(len(country)) # it don't be N cause python is not able to print the lenth of an integer
